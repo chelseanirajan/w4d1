476467032f7bae27e44fabea50588fd9f46c70d3
Nirajan Karki <nirajanchelsea@gmail.com>
2022-11-22 09:17:58 -0600

    se

--- q3/index.js
+++ q3/index.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -38,8 +39,31 @@ const products = [
       "https://assets.adidas.com/images/w_600,f_auto,q_auto/5898799b135648bcaf35ae28011c172b_9366/Ultraboost_DNA_5.0_Shoes_White_GV8740_01_standard.jpg",
   },
 ];
-app.get("/product", (req, res) => {
+const cartItems = [];
+app.get("/", (req, res) => {
   res.render("product", { pageTitle: "Product", prods: products });
+});
+app.post("/addToCart/:id", (req, res) => {
+  const id = +req.params.id;
+  const item = products.find((pro) => pro.id === id);
+  cartItems.push(item);
+  console.log(cartItems);
+  res.render("user-cart", { pageTitle: "My Item", items: cartItems });
+});
+app.get("/cart", (req, res) => {
+  let cart = {};
+  if (req.session.cart) {
+    cart = req.session.cart;
+  }
+  res.render("cart", { pageTitle: "my Title", cart: cart });
+});
+app.post("/addToCart", (req, res) => {
+  const cart = JSON.parse(JSON.stringify(req.obj));
+  req.session.cart = cart;
+  res.redirect("/cart");
+});
+app.use((req, res) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
